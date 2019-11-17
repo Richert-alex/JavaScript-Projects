3949d27f841db4f1ab91f61dec20771ee6c9eeec
Richert-alex <56421121+Richert-alex@users.noreply.github.com>
2019-11-17 13:41:08 -0800

    assignment submission

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function mySecondFunction() {
+    var str = "This text is red!";
+    var result = str.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
